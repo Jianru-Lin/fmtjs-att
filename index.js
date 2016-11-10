9510e95875eb78fbf7922168939f05a23b7bebe1
Jianru Lin <jianru.lin@gmail.com>
2016-11-10 09:19:19 +0800

    使用单独的模块来完成 ast 分析

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var assert = require('assert')
 var translate = require('./lib/translate')
 var fillIndent = require('./lib/fillIndent')
 var translate4html = require('./lib/translate4html')
 
 // 把 AST 转换成 ATT
 function att(ast, options) {
+	assert(ast)	
 	options = fill_default_value(options)
 	switch (options.mode) {
 		case 'text':
@@ -16,7 +18,7 @@ function att(ast, options) {
 
 	function fill_default_value(options) {
 		options = options || {}
-		options.mode = options.mode || 'text'
+		options.mode = options.mode || 'html'
 		return options
 	}
 }
